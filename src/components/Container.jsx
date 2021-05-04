908e2cb87f36cdcc9830cccd5f51adceedb65db7
velizhaar <velizhaar@gmail.com>
2021-05-04 18:42:40 +0700

    first commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+// import { RiAccountCircleFill } from "react-icons/ri"
+
+export default function Container() {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="account">
+          <div className="notification">
+            <h1>HI !</h1>
+            <h2>Your Account is Verified</h2>
+          </div>
+          <div className="user">
+            {/* <RiAccountCircleFill size="4rem" color="purple" /> */}
+            <img src="icons/user.png" alt="user" />
+          </div>
+        </div>
+        <div className="description">
+          <h3>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi
+            quaerat quibusdam tempore temporibus atque dolor nihil veniam quae
+            est officiis molestias, doloribus accusantium laborum culpa nobis
+            numquam magnam in. Tempora voluptatum ex, natus doloribus dolorem
+            esse enim animi quod mollitia nobis impedit, pariatur modi. Aliquam
+            ipsa delectus sed optio. Consectetur quisquam consequuntur sed
+            suscipit? Provident, adipisci molestiae ab facilis dolorum ratione
+            at magnam voluptates numquam eligendi commodi? A tempore tenetur
+            quibusdam corrupti repellat. Dolores, molestias. Non, dolores
+            dolorem sequi ipsa minus beatae sint quos in veritatis assumenda,
+            eaque labore eum repellendus. Provident corrupti soluta sunt sed
+            nulla eius deleniti aliquid dignissimos in at necessitatibus culpa
+            tempore assumenda, amet perspiciatis earum voluptatibus? Itaque
+            numquam molestiae iure maiores. Minima amet sapiente perspiciatis,
+            tenetur voluptatem temporibus voluptate iure deserunt quas iusto
+            vitae incidunt ab fuga beatae autem nihil aliquid blanditiis, fugit
+            dignissimos ratione fugiat alias reiciendis. Quia eum ipsam
+            asperiores minus laudantium ea eveniet ex earum, recusandae suscipit
+            ducimus eius placeat repellat, molestiae fuga.
+          </h3>
+        </div>
+      </div>
+    </div>
+  )
+}
